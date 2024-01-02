f976ff0688b578cd45406a8abe4213fcdea8a7b3
Lander Brandt <landerbrandt@gmail.com>
2024-01-01 20:41:58 -0800

    Add support for loading replay in metadata/data parts

--- parser/src/wowsreplay.rs
+++ parser/src/wowsreplay.rs
@@ -22,6 +22,7 @@ pub struct VehicleInfoMeta {
 pub struct ReplayMeta {
     pub matchGroup: String,
     pub gameMode: u32,
+    pub gameType: String,
     pub clientVersionFromExe: String,
     pub scenarioUiCategoryId: u32,
     pub mapDisplayName: String,
@@ -49,21 +50,22 @@ pub struct ReplayMeta {
 #[derive(Debug)]
 struct Replay<'a> {
     meta: ReplayMeta,
+    raw_meta: &'a str,
     extra_data: Vec<&'a [u8]>,
     decompressed_size: u32,
     compressed_size: u32,
 }
 
-fn decode_meta(meta: &[u8]) -> Result<ReplayMeta, Error> {
-    let meta = std::str::from_utf8(meta)?;
-    let meta: ReplayMeta = serde_json::from_str(meta)?;
-    Ok(meta)
+fn decode_meta(meta: &[u8]) -> Result<(&str, ReplayMeta), Error> {
+    let raw_meta = std::str::from_utf8(meta)?;
+    let meta: ReplayMeta = serde_json::from_str(raw_meta)?;
+    Ok((raw_meta, meta))
 }
 
-fn parse_meta(i: &[u8]) -> IResult<&[u8], ReplayMeta> {
+fn parse_meta(i: &[u8]) -> IResult<&[u8], (&str, ReplayMeta)> {
     let (i, meta_len) = le_u32(i)?;
-    let (i, meta) = take(meta_len)(i)?;
-    let meta = match decode_meta(meta) {
+    let (i, raw_meta) = take(meta_len)(i)?;
+    let meta = match decode_meta(raw_meta) {
         Ok(x) => x,
         Err(e) => {
             return Err(nom::Err::Error(e.into()));
@@ -80,7 +82,7 @@ fn block(i: &[u8]) -> IResult<&[u8], &[u8]> {
 fn replay_format(i: &[u8]) -> IResult<&[u8], Replay> {
     let (i, magic) = le_u32(i)?;
     let (i, block_count) = le_u32(i)?;
-    let (i, meta) = parse_meta(i)?;
+    let (i, (raw_meta, meta)) = parse_meta(i)?;
 
     let (i, blocks) = count(block, (block_count as usize) - 1)(i)?;
     let (i, decompressed_size) = le_u32(i)?;
@@ -89,6 +91,7 @@ fn replay_format(i: &[u8]) -> IResult<&[u8], Replay> {
         i,
         Replay {
             meta: meta,
+            raw_meta,
             extra_data: blocks,
             decompressed_size,
             compressed_size,
@@ -99,10 +102,23 @@ fn replay_format(i: &[u8]) -> IResult<&[u8], Replay> {
 #[derive(Debug)]
 pub struct ReplayFile {
     pub meta: ReplayMeta,
+    pub raw_meta: String,
     pub packet_data: Vec<u8>,
 }
 
 impl ReplayFile {
+    pub fn from_decrypted_parts(meta: Vec<u8>, packet_data: Vec<u8>) -> Result<ReplayFile, Error> {
+        let (_raw_meta, parsed_meta) = decode_meta(meta.as_slice())?;
+
+        let raw_meta = String::from_utf8(meta)?;
+
+        Ok(ReplayFile {
+            meta: parsed_meta,
+            raw_meta,
+            packet_data,
+        })
+    }
+
     pub fn from_file(replay: &std::path::Path) -> Result<ReplayFile, ErrorKind> {
         let mut f = std::fs::File::open(replay).unwrap();
         let mut contents = vec![];
@@ -140,6 +156,7 @@ impl ReplayFile {
 
         Ok(ReplayFile {
             meta: result.meta,
+            raw_meta: result.raw_meta.to_string(),
             packet_data: contents,
         })
     }
